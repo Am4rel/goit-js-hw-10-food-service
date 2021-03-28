fcbe611028d28e8119a4b624d28a47e8127951a9
Oleg Shevchenko <ama.freelnc@gmail.com>
2021-03-28 17:13:34 +0300

    Adding template and js

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dishes from './menu.json';
+// import f from "./templates"
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const themeSwitcher = document.getElementById("theme-switch-toggle");
+const currentTheme = localStorage.getItem("theme");
+const bodyEl = document.querySelector("body");
+
+if (!currentTheme) {
+    localStorage.setItem("theme", Theme.LIGHT)
+    bodyEl.classList.add(Theme.LIGHT);
+} else {
+    bodyEl.classList.add(currentTheme);
+    currentTheme === Theme.LIGHT ? themeSwitcher.checked = false : themeSwitcher.checked = true;
+};
+
+themeSwitcher.addEventListener("change", onThemeSwitcherChange)
+
+function onThemeSwitcherChange() {
+    if (themeSwitcher.checked) {
+        localStorage.setItem("theme", Theme.DARK)
+    } else {
+        localStorage.setItem("theme", Theme.LIGHT)
+    };
+    bodyEl.classList.toggle(Theme.LIGHT);
+    bodyEl.classList.toggle(Theme.DARK);
+};
+
+// console.log(f)
